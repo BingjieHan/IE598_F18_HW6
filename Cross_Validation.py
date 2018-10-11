9bfdd66537a5103f8fc6ac6941e59a23a93295a2
Bingjie Han <42777171+BingjieHan@users.noreply.github.com>
2018-10-11 00:52:35 -0500

    Update Cross_Validation.py

--- Cross_Validation.py
+++ Cross_Validation.py
@@ -1,49 +1,74 @@
-import numpy as np
-from sklearn.cross_validation import train_test_split
-from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
-#training set & test set
-a=[]
-b=[]
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
 
-for r in range(1,11):
-    X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.1, random_state=r, stratify=y)
-    tree = DecisionTreeClassifier(criterion='gini', 
-                              max_depth=4, 
-                              random_state=1)
-    tree.fit(X_train, y_train)
-    y_ptrain=tree.predict(X_train)
-    y_ptest=tree.predict(X_test)
-    a.append(accuracy_score(y_train, y_ptrain))
-    b.append(accuracy_score(y_test,y_ptest)) 
-print(np.mean(a),np.mean(b),np.std(a),np.std(b))
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+        'machine-learning-databases/wine/wine.data',
+         header=None)
 
-#Cross Validation with 10 Folds
-acc_r=[]
+df.columns = ['Class label', 'Alcohol',
+                    'Malic acid', 'Ash',
+                    'Alcalinity of ash', 'Magnesium',
+                    'Total phenols', 'Flavanoids',
+                    'Nonflavanoid phenols',
+                    'Proanthocyanins',
+                    'Color intensity', 'Hue',
+                    'OD280/OD315 of diluted wines',
+                    'Proline']
+
+X, y = df.iloc[:,1:].values, df.iloc[:,0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                      test_size=0.1,
+                      random_state=0,
+                      stratify=y)
+
+n_space=np.arange(1,30,1)
+param_grid = {'n_estimators': n_space}
+
+
 score=[]
-for rr in range(1,11):
-    X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.1, random_state=rr, stratify=y)
-    tree_cross = DecisionTreeClassifier(criterion='gini', 
-                              max_depth=4, 
-                              random_state=1)
-    scores = cross_val_score(estimator=tree_cross, X=X_train, y=y_train, cv=10, n_jobs=1)
-    acc_r.append(np.mean(scores))
-    tree_cross.fit(X_train, y_train)
-    y_pred=tree_cross.predict(X_test)
-    score.append(accuracy_score(y_test, y_pred))
-
-print(acc_r)
+for i in n_space:
+    forest = RandomForestClassifier(criterion='gini',
+                                 n_estimators=i,
+                                 random_state=1,
+                                 n_jobs=2)
+    forest.fit(X_train, y_train)
+    scores = cross_val_score(estimator=forest, X=X_train, y=y_train, cv=10, n_jobs=1)
+    y_pred = forest.predict(X_test)
+    s = accuracy_score(y_test, y_pred)
+    #score.append(np.mean(scores))
+    score.append(s)
 print(score)
-print(np.std(acc_r))
-print(np.std(score))
+    
+
+forest = RandomForestClassifier(criterion='gini',
+                                 random_state=1,
+                                 n_jobs=2)
+forest_cv = GridSearchCV(forest, param_grid, cv=10)
+forest_cv.fit(X_train, y_train)
+print(forest_cv.best_params_)
+#print(forest_cv.cv_results_)
+
+
+
+best_forest = RandomForestClassifier(criterion='gini',
+                                 n_estimators=15,
+                                 random_state=1,
+                                 n_jobs=2)
+
+best_forest.fit(X_train, y_train)
+importances = pd.Series(data=best_forest.feature_importances_,
+                        index= df.columns[1:])
+importances_sorted = importances.sort_values()
+importances_sorted.plot(kind='barh', color='purple')
+plt.title('Features Importances')
+plt.show()
 
 print("My name is Bingjie Han")
 print("My NetID is: bingjie5")
